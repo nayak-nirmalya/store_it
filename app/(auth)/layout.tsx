1d730a0ab8be54eb5ed259b5860a93884421fe09
Nirmalya Nayak <n.nayak263@gmail.com>
2024-11-11 15:34:30 +0530

    add styling to auth-layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -8,13 +8,13 @@ export default function AuthLayout({
 }) {
   return (
     <div className="flex min-h-screen">
-      <section className="bg-brand p-10">
-        <div>
+      <section className="hidden w-1/2 items-center justify-center bg-brand p-10 lg:flex xl:w-2/5">
+        <div className="flex max-h-[800px] max-w-[430px] flex-col justify-center space-y-12">
           <Image
             className="h-auto"
-            src="/favicon.ico"
+            src="/assets/icons/logo-full.svg"
             alt="logo"
-            width={16}
+            width={224}
             height={16}
           />
           <div className="space-y-5 text-white">
@@ -23,9 +23,27 @@ export default function AuthLayout({
               This is a place where you can store all your documents.
             </p>
           </div>
+          <Image
+            className="transition-all hover:rotate-2 hover:scale-105"
+            src="/assets/images/files.png"
+            alt="Files"
+            width={342}
+            height={342}
+          />
         </div>
       </section>
-      {children}
+      <section className="flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0">
+        <div className="mb-16 lg:hidden">
+          <Image
+            className="h-auto w-[200px] lg:w-[250px]"
+            src="/assets/icons/logo-full-brand.svg"
+            alt="logo"
+            width={224}
+            height={82}
+          />
+        </div>
+        {children}
+      </section>
     </div>
   );
 }
