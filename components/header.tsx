fee3441f215d8c508871271ede0402f57c16c9ca
Nirmalya Nayak <n.nayak263@gmail.com>
2024-11-17 09:51:54 +0530

    add new components file: search.tsx & file-uploader.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -1,3 +1,27 @@
+import Image from "next/image";
+
+import { Button } from "./ui/button";
+import { Search } from "./search";
+import { FileUploader } from "./file-uploader";
+
 export function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <Search />
+      <div className="header-wrapper">
+        <FileUploader />
+        <form>
+          <Button type="submit" className="sign-out-button">
+            <Image
+              src="/assets/icons/logout.svg"
+              alt="logo"
+              width={24}
+              height={24}
+              className="w-6"
+            />
+          </Button>
+        </form>
+      </div>
+    </header>
+  );
 }
