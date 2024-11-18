f0721e1a849834bb34094b240fe908a76737f066
Nirmalya Nayak <n.nayak263@gmail.com>
2024-11-18 09:19:49 +0530

    show hamburger menu icon to open sheet on mobile devices

--- components/mobile-navigation.tsx
+++ components/mobile-navigation.tsx
@@ -1,3 +1,50 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+import { usePathname } from "next/navigation";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
 export function MobileNavigation() {
-  return <div>Mobile Navigation</div>;
+  const [open, setOpen] = useState(false);
+  const pathname = usePathname();
+
+  return (
+    <header className="mobile-header">
+      <Image
+        src="/assets/icons/logo-full-brand.svg"
+        alt="logo"
+        width={120}
+        height={52}
+        className="h-auto"
+      />
+      <Sheet open={open} onOpenChange={setOpen}>
+        <SheetTrigger>
+          <Image
+            src="/assets/icons/menu.svg"
+            alt="Search"
+            width={30}
+            height={30}
+          />
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Are you absolutely sure?</SheetTitle>
+            <SheetDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+    </header>
+  );
 }
