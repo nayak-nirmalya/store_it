c80d982688ace3a6b1cb1ed7a03cbe705043cf7b
Nirmalya Nayak <n.nayak263@gmail.com>
2024-11-21 07:31:11 +0530

    add util fn getFileIcon: that returns generic file icon svg

--- components/thumbnail.tsx
+++ components/thumbnail.tsx
@@ -1,11 +1,31 @@
+import Image from "next/image";
+
+import { cn, getFileIcon } from "@/lib/utils";
+
 export function Thumbnail({
   type,
   extension,
-  url,
+  url = "",
+  imageClassName,
+  className,
 }: {
   type: string;
   extension: string;
   url?: string;
+  imageClassName?: string;
+  className?: string;
 }) {
-  return <div>Thumbnail</div>;
+  const isImage = type === "image" && extension !== "svg";
+
+  return (
+    <figure>
+      <Image
+        src={isImage ? url : getFileIcon(extension, type)}
+        alt="thumbnail"
+        width={100}
+        height={100}
+        className={cn("size-8 object-contain", imageClassName)}
+      />
+    </figure>
+  );
 }
