6130ed1000538d114e7c5328afb84e76f8635298
Nirmalya Nayak <n.nayak263@gmail.com>
2024-11-24 10:10:16 +0530

    fetch & render file name

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -1,11 +1,16 @@
 import { Sort } from "@/components/sort";
 
+import { getFiles } from "@/lib/actions/file.actions";
+import { Models } from "node-appwrite";
+
 export default async function Page({
   params,
 }: {
   params: Promise<{ type: string }>;
 }) {
   const type = (await params)?.type || "";
+
+  const files = await getFiles();
 
   return (
     <div className="page-container">
@@ -22,6 +27,17 @@ export default async function Page({
         </div>
       </section>
       {/* Render Files */}
+      {files.total > 0 ? (
+        <section className="file-list">
+          {files.documents.map((file: Models.Document) => (
+            <h1 key={file.$id} className="h1">
+              {file.name}
+            </h1>
+          ))}
+        </section>
+      ) : (
+        <p className="empty-list">No files uploaded</p>
+      )}
     </div>
   );
 }
--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -70,7 +70,7 @@ const createQueries = (currentUser: Models.Document) => {
   const queries = [
     Query.or([
       Query.equal("owner", [currentUser.$id]),
-      Query.equal("users", [currentUser.email]),
+      Query.contains("users", [currentUser.email]),
     ]),
   ];
 
