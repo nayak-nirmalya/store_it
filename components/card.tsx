61e68082847ad543c098bedbb8ebd3ec555192ce
Nirmalya Nayak <n.nayak263@gmail.com>
2024-11-25 20:18:00 +0530

    add new component: FormattedDateTime

--- components/card.tsx
+++ components/card.tsx
@@ -1,5 +1,34 @@
+import Link from "next/link";
 import { Models } from "node-appwrite";
 
+import { Thumbnail } from "@/components/thumbnail";
+import { FormattedDateTime } from "@/components/formatted-datetime";
+
+import { convertFileSize } from "@/lib/utils";
+
 export function Card({ file }: { file: Models.Document }) {
-  return <div>{file.name}</div>;
+  return (
+    <Link href={file.url} target="_blank" className="file-card">
+      <div className="flex justify-between">
+        <Thumbnail
+          type={file.type}
+          extension={file.extension}
+          url={file.url}
+          className="!size-20"
+          imageClassName="!size-11"
+        />
+        <div className="flex flex-col items-end justify-between">
+          {/* ActionsDropdown */}
+          <p className="body-1">{convertFileSize(file.size)}</p>
+        </div>
+      </div>
+      <div className="file-card-details">
+        <p className="subtitle-2 line-clamp-1">{file.name}</p>
+        <FormattedDateTime
+          date={file.$createdAt}
+          className="body-2 text-light-100"
+        />
+      </div>
+    </Link>
+  );
 }
