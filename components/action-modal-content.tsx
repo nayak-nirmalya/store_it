b6975b7e8eb5a87f8ef2d45d2b96a2fe3154bd19
Nirmalya Nayak <n.nayak263@gmail.com>
2024-11-27 14:01:12 +0530

    render DateTime inside file details modal

--- components/action-modal-content.tsx
+++ components/action-modal-content.tsx
@@ -1,11 +1,16 @@
 import { Models } from "node-appwrite";
 
 import { Thumbnail } from "@/components/thumbnail";
+import { FormattedDateTime } from "@/components/formatted-datetime";
 
 function ImageThumbnail({ file }: { file: Models.Document }) {
   return (
     <div className="file-details-thumbnail">
       <Thumbnail type={file.type} extension={file.extension} url={file.url} />
+      <div className="flex flex-col">
+        <p className="subtitle-2 mb-1">{file.name}</p>
+        <FormattedDateTime date={file.$createdAt} className="caption" />
+      </div>
     </div>
   );
 }
