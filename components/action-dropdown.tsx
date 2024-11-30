3da4c3daacd657b91bc3797ff7d014278a438a98
Nirmalya Nayak <n.nayak263@gmail.com>
2024-11-30 19:49:36 +0530

    add handleRemoveUser to remove user

--- components/action-dropdown.tsx
+++ components/action-dropdown.tsx
@@ -77,7 +77,17 @@ export function ActionDropdown({ file }: { file: Models.Document }) {
     setIsLoading(false);
   };
 
-  const handleRemoveUser = () => {};
+  const handleRemoveUser = async (email: string) => {
+    const updatedEmails = emails.filter((e) => e !== email);
+    const success = await updateFileUsers({
+      fileId: file.$id,
+      emails: updatedEmails,
+      path,
+    });
+
+    if (success) setEmails(updatedEmails);
+    closeAllModals();
+  };
 
   const renderDialogContent = () => {
     if (!action) return null;
