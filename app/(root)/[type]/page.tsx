5867fa66e9280dbe7e95c62f4a4ee65173b33496
Nirmalya Nayak <n.nayak263@gmail.com>
2024-12-02 12:18:32 +0530

    show total size of type

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -3,8 +3,12 @@ import { Models } from "node-appwrite";
 import { Sort } from "@/components/sort";
 import { Card } from "@/components/card";
 
-import { getFiles } from "@/lib/actions/file.actions";
-import { getFileTypesParams } from "@/lib/utils";
+import { getFiles, getTotalSpaceUsed } from "@/lib/actions/file.actions";
+import {
+  convertFileSize,
+  getFileTypesParams,
+  getUsageSummary,
+} from "@/lib/utils";
 
 export default async function Page({
   params,
@@ -21,13 +25,20 @@ export default async function Page({
 
   const files = await getFiles({ types, searchText, sort });
 
+  const totalSpace = await getTotalSpaceUsed();
+  const usageSummary = getUsageSummary(totalSpace);
+  const [summary] = usageSummary.filter((summary) =>
+    summary.url.endsWith(type)
+  );
+
   return (
     <div className="page-container">
       <section className="w-full">
         <h1 className="h1 capitalize">{type}</h1>
         <div className="total-size-section">
           <p className="body-1">
-            Total: <span className="h5">34 MB</span>
+            Total:{" "}
+            <span className="h5">{convertFileSize(summary?.size) || 0}</span>
           </p>
           <div className="sort-container">
             <p className="body-1 hidden text-light-200 sm:block">Sort by:</p>
@@ -35,7 +46,6 @@ export default async function Page({
           </div>
         </div>
       </section>
-      {/* Render Files */}
       {files.total > 0 ? (
         <section className="file-list">
           {files.documents.map((file: Models.Document) => (
