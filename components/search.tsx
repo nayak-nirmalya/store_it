611d7c23d7ca8f4ff5d536ffccb08132319d355a
Nirmalya Nayak <n.nayak263@gmail.com>
2024-12-02 10:24:58 +0530

    fetch and render search results

--- components/search.tsx
+++ components/search.tsx
@@ -1,3 +1,86 @@
+"use client";
+
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+import { Models } from "node-appwrite";
+
+import { Input } from "@/components/ui/input";
+
+import { getFiles } from "@/lib/actions/file.actions";
+import { Thumbnail } from "./thumbnail";
+import { FormattedDateTime } from "./formatted-datetime";
+
 export function Search() {
-  return <div>Search</div>;
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState<Models.Document[]>([]);
+  const [open, setOpen] = useState(false);
+
+  const searchParams = useSearchParams();
+  const searchQuery = searchParams.get("query") || "";
+
+  useEffect(() => {
+    const fetchFiles = async () => {
+      const files = await getFiles({ searchText: query });
+      setResults(files.documents);
+      setOpen(true);
+    };
+
+    fetchFiles();
+  }, [query]);
+
+  useEffect(() => {
+    if (!searchQuery) {
+      setQuery("");
+    }
+  }, [searchQuery]);
+
+  return (
+    <div className="search">
+      <div className="search-input-wrapper">
+        <Image
+          src="/assets/icons/search.svg"
+          alt="search"
+          width={24}
+          height={24}
+        />
+        <Input
+          value={query}
+          placeholder="Search..."
+          className="search-input"
+          onChange={(ev) => setQuery(ev.target.value)}
+        />
+        {open && (
+          <ul className="search-result">
+            {results.length > 0 ? (
+              results.map((file) => (
+                <li
+                  className="flex items-center justify-between"
+                  key={file.$id}
+                >
+                  <div className="flex cursor-pointer items-center gap-4">
+                    <Thumbnail
+                      type={file.type}
+                      extension={file.extension}
+                      url={file.url}
+                      className="size-9 min-w-9"
+                    />
+                    <p className="subtitle-2 line-clamp-1 text-light-100">
+                      {file.name}
+                    </p>
+                  </div>
+                  <FormattedDateTime
+                    date={file.$createdAt}
+                    className="caption line-clamp-1 text-light-200"
+                  />
+                </li>
+              ))
+            ) : (
+              <p className="empty-result">No files found</p>
+            )}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
 }
